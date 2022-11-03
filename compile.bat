419088a2f5b4d84603706344b5a7cb4c6eb98900
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2022-11-03 02:32:13 -0400

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -2,6 +2,6 @@ nasm BootLoader.asm -f bin -o BootLoader.bin
 
 nasm ExtendedProgram.asm -f bin -o ExtendedProgram.bin
 
-copy /b BootLoader.bin+ExtendedProgram.bin BootLoader.flp
+copy /b BootLoader.bin+ExtendedProgram.bin os.flp
 
 pause
